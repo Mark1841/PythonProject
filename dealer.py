1e44ec57b0141a8367d045ece1251679d350e1cb
Mark1841 <mjanovitz@outlook.com>
2025-05-17 13:14:22 -0400

    Changed deal take_turn function to make more concise.

--- dealer.py
+++ dealer.py
@@ -8,13 +8,9 @@ class Dealer:
 
 
     def take_turn(self, my_deck):
-        turn_over = False
 
-        while not turn_over:
-            if self.hand.value <17:
-                self.hand.add_card(my_deck.deal())
-            else:
-                turn_over = True
+        while self.hand.value <17:
+            self.hand.add_card(my_deck.deal())
 
-            if self.hand.value > 21:
-                self.bust = True
+        if self.hand.value > 21:
+            self.bust = True
